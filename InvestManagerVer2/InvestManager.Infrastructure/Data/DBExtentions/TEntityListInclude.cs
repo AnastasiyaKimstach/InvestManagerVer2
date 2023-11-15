2619b3ff1a1d9b638bbc50113bdce44615c251b1
akimstach <nkimstac@gmail.com>
2023-11-16 02:42:27 +0300

    Added DBExtentions

---
+++ InvestManagerVer2/InvestManager.Infrastructure/Data/DBExtentions/TEntityListInclude.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace InvestManager.Infrastructure.Data.DBExtentions
+{
+    public static class TEntityListInclude
+    {
+        public static IQueryable<TEntity> IncludeFields<TEntity>(this IQueryable<TEntity> entities, IList<Expression<Func<TEntity, object>>> includeOptions) where TEntity : class
+        {
+            if (includeOptions != null && includeOptions.Count > 0)
+            {
+                var firstOption = includeOptions.First();
+                var query = entities.Include(firstOption);
+                foreach (var item in includeOptions.Skip(1))
+                {
+                    query = query.Include(item);
+                }
+                return query;
+            }
+            return entities;
+        }
+    }
+}
