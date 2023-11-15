ddddc206207fb9ddb2cf26daa7ac750b6843f754
akimstach <nkimstac@gmail.com>
2023-11-16 02:42:07 +0300

    Added IUnitOfWork and fix IRepository

--- InvestManagerVer2/InvestManager.ApplicatoinCore/Interfaces/IRepository.cs
+++ InvestManagerVer2/InvestManager.ApplicatoinCore/Interfaces/IRepository.cs
@@ -1,9 +1,14 @@
 ﻿using InvestManager.ApplicatoinCore.Models;
+using System.Linq.Expressions;
 
 namespace InvestManagerVer2.Web.Interfaces
 {
-    public interface IRepository
+    public interface IRepository<T> where T : class
     {
-        Task<IEnumerable<Client>> GetClientsAsync();
+        //Task<IList<T>> GetAllAsync(QueryEntityOptions<T> options); 
+        Task<T?> GetByIdAsync(int id, params Expression<Func<T, object>>[] includes);
+        Task CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
     }
 }
---
+++ InvestManagerVer2/InvestManager.ApplicatoinCore/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿
+using InvestManager.ApplicatoinCore.Models;
+using InvestManagerVer2.Web.Interfaces;
+
+namespace InvestManager.ApplicatoinCore.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IRepository<Asset> Assets { get; }
+        IRepository<AssetInPortfolio> AssetInPortfolioes { get; }
+        IRepository<AsssetPriceHistory> AsssetPriceHistoryes { get; }
+        IRepository<Category> Categoryes { get; }
+        IRepository<ChangePrice> ChangePrices { get; }
+        IRepository<Client> Clients { get; }
+        IRepository<Country> Countryes { get; }
+        IRepository<EmploymentStatus> EmploymentStatus { get; }
+        IRepository<InvestPortfolio> InvestPortfolioes { get; }
+        IRepository<Statistics> Statistics { get; }
+        IRepository<Transaction> Transactions { get; }
+    }
+}
